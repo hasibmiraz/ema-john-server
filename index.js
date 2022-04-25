7ba8bc3dc0c18e21391921efc6373476a117d654
hasibmiraz <hasibmiraz09@gmail.com>
2022-04-26 00:06:42 +0600

    changed port name

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
 import 'dotenv/config';
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
