fcc728be26df5f2418322267a45b1dc238094cb0
hasibmiraz <hasibmiraz09@gmail.com>
2022-04-26 15:58:48 +0600

    cors modified

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: '*' }));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ema-john-db.mhzvr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
